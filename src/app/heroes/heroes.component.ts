7ea2e5474c3c01c9e8fac44eca51efba68d1e04b
Leonardo F Petrauskas <leonardopetrauskas@hotmail.com>
2018-04-18 16:22:02 -0300

    Backup

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service'
 
 @Component({
@@ -10,22 +9,16 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes : Hero[];
+  heroes: Hero[];
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero) : void{
-    this.selectedHero = hero;
-  }
-
-  getHero(): void {
+  getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    this.getHero();
+    this.getHeroes();
   }
 
 }
